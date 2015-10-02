4cdabf6d07acfa651cfddc8df5fc8e5e82e4a004
paul <zzff8@mail.missouri.edu>
2015-10-01 23:25:21 -0500

    some change

--- src/HexoSkin/Activity.java
+++ src/HexoSkin/Activity.java
@@ -72,7 +72,12 @@ public class Activity {
 					RECORD_ID = RECORD_ID.replaceAll("^\\s+|\\s+$", "");
 					URL = Constants.BASE_URL + PARAMETER +"/?record=" + RECORD_ID ;
 					fileName = RECORD_ID +"_"+ PARAMETER +".csv" ;
-					
+					try {
+						Thread.sleep(200);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 					startExecution(reco);
 				}
 			}
